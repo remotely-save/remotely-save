da180fcc92099df0e22a605f19be87dd8a5efa60
fyears <hjwdhjwd@gmail.com>
2021-11-02 10:07:00 +0800

    overwrite buffer

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
@@ -9,6 +10,16 @@ module.exports = {
     path: __dirname,
     libraryTarget: "commonjs",
   },
+  plugins: [
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
   module: {
     rules: [
       {
